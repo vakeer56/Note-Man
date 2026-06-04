e555d25121fc8c1e17cdd2539731550a0abd07ca
vakeer56 <mailvarun17@gmail.com>
2026-06-04 11:56:30 +0530

    chore: initial commit

---
+++ server/README.md
@@ -0,0 +1,1 @@
+# Backend of Note Man
---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port $${PORT}...`);
+})
