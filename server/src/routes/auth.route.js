3e1a6573bc9c888e7f41ebb7bda81ea64e26a4b8
vakeer56 <mailvarun17@gmail.com>
2026-06-09 17:33:17 +0530

    fix: protect note routes using auth middleware

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import passport from 'passport';
 
+
 const router = express.Router();
 
 router.get(
--- server/src/routes/note.route.js
+++ server/src/routes/note.route.js
@@ -9,16 +9,18 @@ import {
     searchNotes
 } from "../controllers/note.controlller.js";
 
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
+
 import express from 'express';
 const router = express.Router();
 
-router.post('/create', createNote);
-router.get('/all', getAllNotes);
-router.get('/search', searchNotes)
-router.get('/tag/:tag', getNotesByTag);
-router.get('/getarchived', getArchivedNotes);
-router.get('/:noteId', getNoteById);
-router.delete('/delete/:noteId',deleteNote );
-router.patch('/update/:noteId', updateNote);
+router.post('/create',isAuthenticated, createNote);
+router.get('/all',isAuthenticated,  getAllNotes);
+router.get('/search',isAuthenticated,  searchNotes)
+router.get('/tag/:tag',isAuthenticated,  getNotesByTag);
+router.get('/getarchived',isAuthenticated,  getArchivedNotes);
+router.get('/:noteId',isAuthenticated,  getNoteById);
+router.delete('/delete/:noteId',isAuthenticated, deleteNote );
+router.patch('/update/:noteId',isAuthenticated,  updateNote);
 
 export default router;
